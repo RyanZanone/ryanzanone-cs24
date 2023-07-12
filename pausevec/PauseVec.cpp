e43db62f57c8ce6375b51a655ea0894ca48c8f26
Ryan Zanone <ryanzanone@169-231-140-200.wireless.ucsb.edu>
2023-07-11 19:37:10 -0700

    Added stubs

--- pausevec/PauseVec.cpp
+++ pausevec/PauseVec.cpp
@@ -1,10 +1,34 @@
 #include "PauseVec.h"
-#include <iostream>
 
 using namespace std;
 
-int main()
+size_t PauseVec::capacity() const
 {
+    return 1111;
+}
 
-    return 0;
+size_t PauseVec::count() const
+{
+    return 1111;
+}
+
+void PauseVec::push()
+{
+}
+
+int PauseVec::lookup()
+{
+    return 1111;
+}
+
+void PauseVec::mutate(size_t idx, int val)
+{
+}
+
+int PauseVec::remove(size_t idx)
+{
+}
+
+void PauseVec::remove_val(int val)
+{
 }
