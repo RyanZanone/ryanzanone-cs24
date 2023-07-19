ee7297d13613ebf75a929017965705cc682ad438
Ryan Zanone <ryanzanone@169-231-140-200.wireless.ucsb.edu>
2023-07-19 00:38:25 -0700

    Implemented node member functions

--- chunkylist/MyChunkyNode.cpp
+++ chunkylist/MyChunkyNode.cpp
@@ -1,8 +1,28 @@
 #include "MyChunkyNode.h"
 
-MyChunkyNode::MyChunkyNode()
-{
+MyChunkyNode::MyChunkyNode() {
     chunk = new std::string;
     prev_ref = nullptr;
     next_ref = nullptr;
+    num_items = 0;
+}
+
+MyChunkyNode::~MyChunkyNode() {
+    delete[] chunk;
+}
+
+int MyChunkyNode::count() const {
+    return num_items;
+}
+
+std::string* MyChunkyNode::items() const {
+    return chunk;
+}
+
+MyChunkyNode* MyChunkyNode::prev() const {
+    return prev_ref;
+}
+
+MyChunkyNode* MyChunkyNode::next() const {
+    return next_ref;
 }
