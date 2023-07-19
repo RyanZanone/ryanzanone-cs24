181302777e5a72839bddf3a213d774a21be3d8d2
Ryan Zanone <ryanzanone@169-231-140-200.wireless.ucsb.edu>
2023-07-19 00:10:56 -0700

    Fixed formatting

--- chunkylist/ChunkyList.cpp
+++ chunkylist/ChunkyList.cpp
@@ -1,8 +1,7 @@
 #include "ChunkyList.h"
 #include "MyChunkyList.h"
 
-ChunkyList *ChunkyList::create(int chunksize)
-{
+ChunkyList *ChunkyList::create(int chunksize) {
   // This will work if you use the obvious implementation.
   // Change it if you implement a different constructor!
   return new MyChunkyList(chunksize);
