cda6633c8b1c2fb16277446bba7dc13e6f07dee5
Ryan Zanone <ryanzanone@169-231-140-200.wireless.ucsb.edu>
2023-07-19 00:43:08 -0700

    Replaced ChunkyList.cpp

--- chunkylist/MyChunkyList.cpp
+++ chunkylist/MyChunkyList.cpp
@@ -1,20 +1,19 @@
 #include "MyChunkyList.h"
 
-MyChunkyList::MyChunkyList(int chunksize)
-{
+MyChunkyList::MyChunkyList(int chunksize) {
     this->chunksize = chunksize;
     num_items = 0;
     head_ref = nullptr;
     tail_ref = nullptr;
 }
 
-MyChunkyList::~MyChunkyList()
-{
+MyChunkyList::~MyChunkyList() {
     delete head_ref;
     delete tail_ref;
 }
 
-int MyChunkyList::count() const
-{
+int MyChunkyList::count() const {
     return num_items;
 }
+
+
