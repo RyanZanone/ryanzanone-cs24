19e7097e2de85065b35bef4cb76c4fd660bc1ec1
Ryan Zanone <ryanzanone@169-231-140-200.wireless.ucsb.edu>
2023-07-20 02:26:58 -0700

    Implemented split and merge

--- chunkylist/MyChunkyNode.cpp
+++ chunkylist/MyChunkyNode.cpp
@@ -44,24 +44,41 @@ void MyChunkyNode::insert(int index, const std::string &item) {
 }
 
 void MyChunkyNode::split() {
-    // Create new nodes
-    MyChunkyNode* half1 = new MyChunkyNode(chunksize);
-    MyChunkyNode* half2 = new MyChunkyNode(chunksize);
+    // Create new node
+    MyChunkyNode* newnode = new MyChunkyNode(chunksize);
     // Restructure node order
-    half1->prev_ref = this->prev_ref;
-    half1->next_ref = half2;
-    half2->prev_ref = half1;
-    half2->next_ref = this->next_ref;
+    newnode->prev_ref = this;
+    newnode->next_ref = this->next_ref;
     // transfer over data
-
+    int splitindex = num_items / 2;
+    int remaining_items = num_items - splitindex;
+    for(int i = splitindex; i < num_items; i++) {
+        newnode->insert(i - splitindex, chunk[i]);
+        chunk[i] = "";
+        num_items -= 1;
+    }
 }
 
 void MyChunkyNode::merge() {
     if(prev_ref->count() <= chunksize / 2) {
-
+        int mergeindex = chunksize / 2;
+        for (int i = mergeindex; i < chunksize; i++) {
+            chunk[i] = prev_ref->chunk[chunksize - mergeindex];
+        }
+        prev_ref->prev_ref->next_ref = this;
+        MyChunkyNode* tempnode = prev_ref;
+        prev_ref = prev_ref->prev_ref;
+        delete[] tempnode;
     }
     else if(next_ref->count() <= chunksize / 2) {
-
+        int mergeindex = chunksize / 2;
+        for (int i = mergeindex; i < chunksize; i++) {
+            chunk[i] = next_ref->chunk[chunksize - mergeindex];
+        }
+        next_ref->next_ref->prev_ref = this;
+        MyChunkyNode* tempnode = next_ref;
+        next_ref = next_ref->next_ref;
+        delete[] tempnode;
     }
 
 }
@@ -80,4 +97,8 @@ void MyChunkyNode::shift_insert(int start) {
         chunk[i - 1] = "";
         chunk[i] = tempitem;
     }
+}
+
+void MyChunkyNode::list_restructure(MyChunkyNode* currnode) {
+
 }
