bb60aeed546677f170fd2fcd083a4a3255ad3f28
Ryan Zanone <ryanzanone@169-231-140-200.wireless.ucsb.edu>
2023-08-13 00:09:45 -0700

    Changed wordlist

--- spellchecker/WordList.cpp
+++ spellchecker/WordList.cpp
@@ -3,6 +3,22 @@
 #include <fstream>
 #include <cmath>
 
+WordList::WordList(std::istream& stream) {
+    std::string line;
+    while (std::getline(stream, line)) {
+        bool isLowerCase = true;
+        for (char c : line) {
+            if (!std::islower(c)) {
+                isLowerCase = false;
+                break;
+            }
+        }
+        if (isLowerCase) {
+            mWords.push_back(line);
+        }
+    }
+}
+
 Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const {
     Heap suggestions(maxcount);
 
